ab1ee8748b24b6bc4528b4df6fc59d8c61ddcd0d
melvin <melvinn721@gmail.com>
2022-01-01 00:43:20 +1300

    all tests passed

--- src/Countdown.js
+++ src/Countdown.js
@@ -9,32 +9,28 @@ export default function Countdown() {
     const [timer, setTimer] = useState(25 * 60)
     const [timerRunning, setTimerRunning] = useState(false)
     const [timerSet, setTimerSet] = useState(false)
-    const [swap, setSwap] = useState(false)
  
     const beep = document.getElementById('beep')
 
-    //useEffect to countdown times
+    //useEffect to countdown times, switch between times when timer hits 0
     useEffect(() => {     
-        console.log(currentTimer)   
         if(timerRunning && timer > 0) {
             const interval = setInterval(() => setTimer(timer => timer - 1), 1000)
             return () => clearInterval(interval)
         } else if (timerRunning && timer <= 0) {
             beep.play()
-            if (currentTimer === 'Session') {
-                setCurrentTimer('Break')
-                //label.textContent = 'Break'
-                setTimer(breakTime * 60)
-            } 
-            if (currentTimer === 'Break') {
-                console.log('aite')
-                //label.textContent = 'Session'
-                setCurrentTimer('Session')
+            if (currentTimer === 'Break') {                
                 setTimer(sessionTime * 60)
+                setCurrentTimer('Session')
+                setBreakTime(0)
                 return
             } 
-            }
-    }, [timerRunning, timer])
+            if (currentTimer === 'Session') {       
+                setTimer(breakTime * 60)
+                setCurrentTimer('Break')
+                setSessionTime(0)
+                return                
+        }}}, [timerRunning, timer])
     
 
     //set a min and max from times
@@ -59,48 +55,8 @@ export default function Countdown() {
         return seconds < 10 ?  `${minutes}:0${seconds}` : `${minutes}:${seconds}`
     }
     
-    //pass the break or session to the timer
-    // useEffect(() => {
-    //     if (currentTimer === 'Session') {
-    //         setTimer(sessionTime * 60)
-    //         console.log('feou')
-    
-    //     } else if (currentTimer === 'Break') {
-    //         setTimer(breakTime * 60)
-    //     }
-    // }, [currentTimer]) 
-
-    //stop at 0, change to other session type
-    
-        // if (timer < 0 && timerRunning) {
-        //     if (currentTimer === 'Session') {
-        //         setCurrentTimer('Break')
-        //         //setTimer(breakTime * 60)
-        //     } else if (currentTimer === 'Break') {
-        //         setCurrentTimer('Session')
-        //         //setTimer(sessionTime * 60)
-        //     }
-        // }
-        
-    
-        
-           
-    
-    // useEffect(() => {
-    //     console.log('ibf')
-    //     if (currentTimer === 'Session' && !timerSet && !resetPressed ) {
-    //         setTimer(sessionTime * 60)
-    //         console.log('feou')
-    
-    //     } else if (currentTimer === 'Break') {
-    //         setTimer(breakTime * 60)
-    //         console.log('ahh')
-    //     } 
-    // }, [])
-    
-    
-    const resetTimer = () => {
-        
+    //reset states
+    const resetTimer = () => {     
         setTimerRunning(false)
         setTimerSet(false)
         setSessionTime(25)
@@ -109,10 +65,9 @@ export default function Countdown() {
         setCurrentTimer('Session')
         document.getElementById('beep').pause()
         document.getElementById('beep').currentTime = 0
-
     }
 
-    
+   
     return (
         <>
         <h1>Pomodoro Timer</h1>
@@ -124,6 +79,29 @@ export default function Countdown() {
             setTimerSet(true)}}>
             Start</button>
         <button id='reset' onClick={resetTimer}>Reset</button>
+        <div id='session'>
+            <h2 id='session-label'> Session Length</h2>
+            <h3 id='session-length'>{sessionTime}</h3>
+            <button id='session-increment' onClick={() => {
+                if (!timerRunning) {
+                    setSessionTime(sessionTime => sessionTime + 1)
+                    if(!timerSet && currentTimer === 'Session') {
+                        setTimer(timer => (sessionTime + 1) * 60)
+                }}
+                    }}>Increase</button>
+            <button id='session-decrement' onClick={() => {
+                if (!timerRunning) {
+                    setSessionTime(sessionTime => sessionTime - 1)
+                    if(!timerSet && currentTimer === 'Session') {
+                        if (sessionTime === 1) {
+                            setTimer(1 * 60)
+                        } else {
+                            setTimer((sessionTime - 1) * 60)}}
+                        }
+                        
+                    }}
+                >Decrease</button>
+            
         <div id='break'>
             <h2 id='break-label'>Break Length</h2>
             <h3 id='break-length'>{breakTime}</h3>
@@ -138,28 +116,13 @@ export default function Countdown() {
             if (!timerRunning) {  
             setBreakTime(breakTime => breakTime - 1)
             if (!timerSet && currentTimer === 'Break') {
-                setTimer(timer => (breakTime - 1) * 60)}}
-            }}
+                if (breakTime === 1) {
+                    setTimer(1 * 60)
+                } else {
+                setTimer((breakTime - 1) * 60)}}
+            }}}
             >Decrease</button>
         </div>
-        <div id='session'>
-            <h2 id='session-label'>Session Length</h2>
-            <h3 id='session-length'>{sessionTime}</h3>
-            <button id='session-increment' onClick={() => {
-                if (!timerRunning) {
-                    setSessionTime(sessionTime => sessionTime + 1)
-                    if(!timerSet && currentTimer === 'Session') {
-                        setTimer(timer => (sessionTime + 1) * 60)
-                }}
-                    }}>Increase</button>
-            <button id='session-decrement' onClick={() => {
-                if (!timerRunning) {
-                    setSessionTime(sessionTime => sessionTime - 1)
-                    if(!timerSet && currentTimer === 'Session') {
-                        setTimer(timer => (sessionTime - 1) * 60)}}
-                    }}
-                >Decrease</button>
-            
         </div>
 
         </>
